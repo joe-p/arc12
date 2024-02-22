b7138103d70b6129112666e6f126726ee5eff27c
Joe Polny <joepolny@gmail.com>
2024-02-22 18:45:20 -0500

    rekey with tests

--- contracts/__test__/arc12.test.ts
+++ contracts/__test__/arc12.test.ts
@@ -1,12 +1,15 @@
 import { describe, test, expect, beforeAll, beforeEach } from '@jest/globals';
 import { algorandFixture } from '@algorandfoundation/algokit-utils/testing';
-import { Arc12Client } from '../contracts/clients/Arc12Client';
 import * as algokit from '@algorandfoundation/algokit-utils';
+import algosdk from 'algosdk';
+import { Arc12Client } from '../contracts/clients/ARC12Client';
 
 const fixture = algorandFixture();
 algokit.Config.configure({ populateAppCallResources: true });
 
 let appClient: Arc12Client;
+let assetId: number;
+let alice: algosdk.Account;
 
 describe('Arc12', () => {
   beforeEach(fixture.beforeEach);
@@ -24,20 +27,72 @@ describe('Arc12', () => {
       algod
     );
 
+    const assetCreate = algosdk.makeAssetCreateTxnWithSuggestedParamsFromObject({
+      from: testAccount.addr,
+      total: 100,
+      decimals: 0,
+      defaultFrozen: false,
+      suggestedParams: await algod.getTransactionParams().do(),
+    });
+
+    const atc = new algosdk.AtomicTransactionComposer();
+
+    atc.addTransaction({ txn: assetCreate, signer: algosdk.makeBasicAccountTransactionSigner(testAccount) });
+
+    const result = await algokit.sendAtomicTransactionComposer({ atc }, algod);
+
+    assetId = Number(result.confirmations![0].assetIndex);
     await appClient.create.createApplication({});
+
+    await appClient.appClient.fundAppAccount({ amount: algokit.microAlgos(200_000) });
+
+    alice = testAccount;
   });
 
-  test('sum', async () => {
-    const a = 13;
-    const b = 37;
-    const sum = await appClient.doMath({ a, b, operation: 'sum' });
-    expect(sum.return?.valueOf()).toBe(BigInt(a + b));
+  test('Brand new account getAssetSendInfo', async () => {
+    const res = await appClient.getAssetSendInfo({ asset: assetId, receiver: algosdk.generateAccount().addr });
+
+    const itxns = res.return![0];
+    const mbr = res.return![1];
+
+    expect(itxns).toBe(5n);
+    expect(mbr).toBe(228_100n);
   });
 
-  test('difference', async () => {
-    const a = 13;
-    const b = 37;
-    const diff = await appClient.doMath({ a, b, operation: 'difference' });
-    expect(diff.return?.valueOf()).toBe(BigInt(a >= b ? a - b : b - a));
+  test('routerOptIn', async () => {
+    await appClient.optRouterIn({ asa: assetId }, { sendParams: { fee: algokit.microAlgos(2_000) } });
+  });
+
+  test('Brand new account sendAsset', async () => {
+    const { algod } = fixture.context;
+    const bob = algosdk.generateAccount();
+    const sendInfo = (await appClient.getAssetSendInfo({ asset: assetId, receiver: bob.addr })).return;
+
+    const itxns = sendInfo![0];
+    const mbr = sendInfo![1];
+
+    const axfer = algosdk.makeAssetTransferTxnWithSuggestedParamsFromObject({
+      from: alice.addr,
+      to: (await appClient.appClient.getAppReference()).appAddress,
+      assetIndex: assetId,
+      amount: 1,
+      suggestedParams: await algod.getTransactionParams().do(),
+    });
+
+    const mbrPayment = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
+      from: alice.addr,
+      to: (await appClient.appClient.getAppReference()).appAddress,
+      amount: mbr,
+      suggestedParams: await algod.getTransactionParams().do(),
+    });
+
+    await appClient
+      .compose()
+      .addTransaction({ transaction: mbrPayment, signer: alice })
+      .sendAsset(
+        { axfer, receiver: bob.addr },
+        { sendParams: { fee: algokit.microAlgos(1000 + 1000 * Number(itxns)) } }
+      )
+      .execute();
   });
 });
--- contracts/contracts/arc12.algo.ts
+++ contracts/contracts/arc12.algo.ts
@@ -4,200 +4,154 @@
 // eslint-disable-next-line import/no-unresolved, import/extensions
 import { Contract } from '@algorandfoundation/tealscript';
 
-// eslint-disable-next-line no-unused-vars
-class Vault extends Contract {
-  creator = GlobalStateKey<Address>();
-
-  master = GlobalStateKey<AppID>();
-
-  owner = GlobalStateKey<Address>();
-
-  funderMap = BoxMap<AssetID, Address>();
-
-  private closeAcct(): void {
-    /// Send the MBR to the vault creator
+class ControlledAddress extends Contract {
+  @allow.create('DeleteApplication')
+  new(authAddr: Address): Address {
     sendPayment({
-      receiver: this.creator.value,
-      amount: globals.currentApplicationAddress.minBalance,
-      closeRemainderTo: this.owner.value,
+      rekeyTo: authAddr,
     });
 
-    const deleteVaultTxn = this.txnGroup[this.txn.groupIndex + 1];
-    /// Ensure Master.deleteVault is being called for this vault
-    assert(deleteVaultTxn.applicationID === this.master.value);
-    assert(deleteVaultTxn.applicationArgs[0] === method('deleteVault(application,account)void'));
-    assert(deleteVaultTxn.applications[1] === this.app);
-  }
-
-  @allow.create('NoOp')
-  create(owner: Address, sender: Address): void {
-    this.creator.value = sender;
-    this.owner.value = owner;
-    this.master.value = globals.callerApplicationID;
-  }
-
-  reject(asa: AssetID): void {
-    assert(this.txn.sender === this.owner.value);
-    const feeSink = addr('Y76M3MSY6DKBRHBL7C3NNDXGS5IIMQVQVUAB6MP4XEMMGVF2QWNPL226CA')
-    const preMbr = globals.currentApplicationAddress.minBalance;
-
-    /// Send asset back to creator since they are guranteed to be opted in
-    sendAssetTransfer({
-      assetReceiver: asa.creator,
-      xferAsset: asa,
-      assetAmount: 0,
-      assetCloseTo: asa.creator,
-    });
-
-    this.funderMap(asa).delete();
-
-    const mbrAmt = preMbr - globals.currentApplicationAddress.minBalance;
-
-    /// Send MBR to fee sink
-    sendPayment({
-      receiver: feeSink,
-      amount: mbrAmt - this.txn.fee,
-    });
-
-    /// Send fee back to sender
-    sendPayment({
-      receiver: this.txn.sender,
-      amount: this.txn.fee,
-    });
-
-    if (globals.currentApplicationAddress.totalAssets === 0) this.closeAcct();
-  }
-
-  optIn(asa: AssetID, mbrPayment: PayTxn): void {
-    assert(!this.funderMap(asa).exists);
-    assert(mbrPayment.sender === this.txn.sender);
-    assert(mbrPayment.receiver === globals.currentApplicationAddress);
-
-    const preMbr = globals.currentApplicationAddress.minBalance;
-
-    this.funderMap(asa).value = this.txn.sender;
-
-    /// Opt vault into asa
-    sendAssetTransfer({
-      assetReceiver: globals.currentApplicationAddress,
-      assetAmount: 0,
-      xferAsset: asa,
-    });
-
-    assert(mbrPayment.amount === globals.currentApplicationAddress.minBalance - preMbr);
-  }
-
-  claim(asa: AssetID): void {
-    assert(this.funderMap(asa).exists);
-    assert(this.txn.sender === this.owner.value);
-
-    const initialMbr = globals.currentApplicationAddress.minBalance;
-
-    this.funderMap(asa).delete();
-
-    /// Transfer all of the asset to the receiver
-    sendAssetTransfer({
-      assetReceiver: this.txn.sender,
-      assetAmount: globals.currentApplicationAddress.assetBalance(asa),
-      xferAsset: asa,
-      assetCloseTo: this.txn.sender,
-    });
-
-    /// Send MBR to the funder
-    sendPayment({
-      receiver: this.funderMap(asa).value,
-      amount: initialMbr - globals.currentApplicationAddress.minBalance,
-    });
-
-    if (globals.currentApplicationAddress.totalAssets === 0) this.closeAcct();
-  }
-
-  @allow.bareCall('DeleteApplication')
-  delete(): void {
-    assert(!globals.currentApplicationAddress.isInLedger);
-    assert(this.txn.sender === globals.creatorAddress);
+    return this.app.address;
   }
 }
 
-// eslint-disable-next-line no-unused-vars
-class Master extends Contract {
-  vaultMap = BoxMap<Address, AppID>();
+export class ARC12 extends Contract {
+  vaults = BoxMap<Address, Address>();
 
-  @allow.bareCreate()
-  create() { }
+  /**
+   * Opt the ARC12 router into the ASA. This is required before this app can be used to send the ASA to anyone.
+   *
+   * @param asa The ASA to opt into
+   */
+  optRouterIn(asa: AssetID): void {
+    sendAssetTransfer({
+      assetReceiver: this.app.address,
+      assetAmount: 0,
+      xferAsset: asa,
+    });
+  }
 
-  createVault(owner: Address, mbrPayment: PayTxn): AppID {
-    assert(!this.vaultMap(owner).exists);
-    assert(mbrPayment.receiver === globals.currentApplicationAddress);
-    assert(mbrPayment.sender === this.txn.sender);
+  /**
+   * Gets an existing or create a vault for the given address
+   */
+  private getOrCreateVault(addr: Address): Address {
+    if (this.vaults(addr).exists) return this.vaults(addr).value;
 
-    const preCreateMBR = globals.currentApplicationAddress.minBalance;
-
-    /// Create the vault
-    sendMethodCall<typeof Vault.prototype.create>({
-      methodArgs: [owner, this.txn.sender],
-      clearStateProgram: Vault.clearProgram(),
-      approvalProgram: Vault.approvalProgram(),
-      globalNumByteSlice: Vault.schema.global.numByteSlice,
-      globalNumUint: Vault.schema.global.numUint,
+    const vault = sendMethodCall<typeof ControlledAddress.prototype.new>({
+      methodArgs: [this.app.address],
+      onCompletion: OnCompletion.DeleteApplication,
+      approvalProgram: ControlledAddress.approvalProgram(),
+      clearStateProgram: ControlledAddress.clearProgram(),
     });
 
-    const vault = this.itxn.createdApplicationID;
-
-    /// Fund the vault with account MBR
-    sendPayment({
-      receiver: vault.address,
-      amount: globals.minBalance,
-    });
-
-    this.vaultMap(owner).value = vault;
-
-    assert(mbrPayment.amount === (globals.currentApplicationAddress.minBalance - preCreateMBR) + globals.minBalance);
+    this.vaults(addr).value = vault;
 
     return vault;
   }
 
-  verifyAxfer(owner: Address, vaultAxfer: AssetTransferTxn, vault: AppID): void {
-    assert(this.vaultMap(owner).exists);
+  /**
+   *
+   * @param receiver The address to send the asset to
+   * @param asset The asset to send
+   *
+   * @returns The number of itxns sent and the MBR required to send the asset to the receiver
+   */
+  getAssetSendInfo(receiver: Address, asset: AssetID): { itxns: uint64; mbr: uint64 } {
+    const info: { itxns: uint64; mbr: uint64 } = { itxns: 1, mbr: 0 };
 
-    assert(this.vaultMap(owner).value === vault);
-    assert(vaultAxfer.assetReceiver === vault.address);
-    assert(vaultAxfer.assetCloseTo === globals.zeroAddress);
+    if (receiver.isOptedInToAsset(asset)) return info;
+
+    if (!this.vaults(receiver).exists) {
+      /// Two itxns to create vault (create + rekey)
+      /// One itxns to send MBR
+      /// One itxn to opt in
+      info.itxns += 4;
+
+      /// Calculate the MBR for the vault box
+      const preMBR = globals.currentApplicationAddress.minBalance;
+      this.vaults(receiver).value = globals.zeroAddress;
+      const boxMbrDelta = globals.currentApplicationAddress.minBalance - preMBR;
+      this.vaults(receiver).delete();
+
+      /// MBR = MBR for the box + min balance for the vault + ASA MBR
+      info.mbr = boxMbrDelta + globals.minBalance + globals.assetOptInMinBalance;
+
+      return info;
+    }
+
+    const vault = this.vaults(receiver).value;
+
+    if (!vault.isOptedInToAsset(asset)) {
+      /// One itxn to opt in
+      info.itxns += 1;
+
+      if (!(vault.balance >= vault.minBalance + globals.assetOptInMinBalance)) {
+        /// One itxn to send MBR
+        info.itxns += 1;
+
+        /// MBR = ASA MBR
+        info.mbr = globals.assetOptInMinBalance;
+      }
+    }
+
+    return info;
   }
 
-  hasVault(owner: Address): uint64 {
-    // @ts-expect-error Need to fix the return type for .exists in TEALScript
-    return this.vaultMap(owner).exists;
-  }
-
-  getVaultId(owner: Address): AppID {
-    return this.vaultMap(owner).value;
-  }
-
-  getVaultAddr(owner: Address): Address {
-    return this.vaultMap(owner).value.address;
-  }
-
-  deleteVault(vault: AppID): void {
-    /// The fee needs to be 0 because all of the fees need to paid by the vault call
-    /// This ensures the sender will be refunded for all fees if they are rejecting the last ASA
-    assert(this.txn.fee === 0);
-    assert(vault === this.vaultMap(this.txn.sender).value);
-
-    const preDeleteMBR = globals.currentApplicationAddress.minBalance;
-
-    /// Call delete on the vault
-    sendAppCall({
-      applicationID: vault,
-      onCompletion: OnCompletion.DeleteApplication,
+  /**
+   * Send an asset to the receiver
+   *
+   * @param receiver The address to send the asset to
+   * @param axfer The asset transfer to this app
+   *
+   * @returns The address that the asset was sent to (either the receiver or their vault)
+   */
+  sendAsset(receiver: Address, axfer: AssetTransferTxn): Address {
+    verifyAssetTransferTxn(axfer, {
+      assetReceiver: this.app.address,
     });
 
-    this.vaultMap(this.txn.sender).delete();
+    /// If the receiver is opted in, send directly to their account
+    if (receiver.isOptedInToAsset(axfer.xferAsset)) {
+      sendAssetTransfer({
+        assetReceiver: receiver,
+        assetAmount: axfer.assetAmount,
+        xferAsset: axfer.xferAsset,
+      });
 
-    /// Send the MBR back to the vault creator
-    sendPayment({
-      receiver: vault.globalState('creator') as Address,
-      amount: preDeleteMBR - globals.currentApplicationAddress.minBalance,
+      return receiver;
+    }
+
+    const vaultExisted = this.vaults(receiver).exists;
+    const vault = this.getOrCreateVault(receiver);
+
+    if (!vault.isOptedInToAsset(axfer.xferAsset)) {
+      let vaultMbrDelta = globals.assetOptInMinBalance;
+      if (!vaultExisted) vaultMbrDelta += globals.minBalance;
+
+      /// Ensure the vault has enough balance to opt in
+      if (vault.balance < vault.minBalance + vaultMbrDelta) {
+        sendPayment({
+          receiver: vault,
+          amount: vaultMbrDelta,
+        });
+      }
+
+      /// Opt the vault in
+      sendAssetTransfer({
+        sender: vault,
+        assetReceiver: vault,
+        assetAmount: 0,
+        xferAsset: axfer.xferAsset,
+      });
+    }
+
+    /// Transfer the asset to the vault
+    sendAssetTransfer({
+      assetReceiver: vault,
+      assetAmount: axfer.assetAmount,
+      xferAsset: axfer.xferAsset,
     });
+
+    return vault;
   }
 }
